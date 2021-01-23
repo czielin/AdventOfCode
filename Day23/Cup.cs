218b6e17a8be69f9b18ff6dc4264b836ab83cce1
Chris Zielin <chris@zielin.com>
2021-01-22 23:36:34 -0800

    Day 23, part 2.

---
+++ Day23/Cup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23
+{
+    public class Cup
+    {
+        public int Value { get; set; }
+        public Cup Next { get; set; }
+        public Cup Previous { get; set; }
+    }
+}
--- Day23/Program.cs
+++ Day23/Program.cs
@@ -10,58 +10,124 @@ namespace Day23
     {
         static async Task Main(string[] args)
         {
-            var lines = await File.ReadAllLinesAsync("sample_input.txt");
+            var lines = await File.ReadAllLinesAsync("input.txt");
 
             var cups = lines[0].Select(l => int.Parse(l.ToString())).ToList();
 
-            var part1 = MoveCups(cups, 100);
+            //var part1 = MoveCups(cups, 100);
 
-            Console.WriteLine($"The current cup order is: {string.Join("", part1)}");
+            //Console.WriteLine($"The current cup order is: {string.Join("", part1)}");
+
+            Cup cupOne = MoveCupsFaster(cups, 100);
+
+            Console.Write($"The current cup order is: ");
+
+            Cup currentCup = cupOne.Next;
+            while (currentCup != cupOne)
+            {
+                Console.Write(currentCup.Value);
+                currentCup = currentCup.Next;
+            }
+            Console.WriteLine();
 
             for (int count = cups.Max() + 1; count <= 1000000; count++)
             {
                 cups.Add(count);
             }
 
-            var part2 = MoveCups(cups, 10000000);
+            //var part2 = MoveCups(cups, 10000000);
 
-            Console.WriteLine($"The current cup order is: {string.Join("", part2)}");
+            //Console.WriteLine($"The current cup order is: {string.Join("", part2)}");
+
+            cupOne = MoveCupsFaster(cups, 10000000);
+
+            Console.WriteLine($"The part 2 product is: { ((long)cupOne.Next.Value) * ((long)cupOne.Next.Next.Value) }");
         }
 
+        // Second attempt to calculate part 2 faster.
+        private static Cup MoveCupsFaster(List<int> originalCups, int numberOfMoves)
+        {
+            Dictionary<int, Cup> cups = new Dictionary<int, Cup>();
+            Cup previousCup = null;
+            foreach (int cupValue in originalCups)
+            {
+                Cup cup = new Cup { Value = cupValue };
+                cups.Add(cupValue, cup);
+                if (previousCup != null)
+                {
+                    cup.Previous = previousCup;
+                    previousCup.Next = cup;
+                }
+                previousCup = cup;
+            }
+            Cup firstCup = cups[originalCups.First()];
+            firstCup.Previous = previousCup;
+            previousCup.Next = firstCup;
+
+            Cup currentCup = firstCup;
+            int minValue = originalCups.Min();
+            int maxValue = originalCups.Max();
+            for (int moves = 1; moves <= numberOfMoves; moves++)
+            {
+                Cup nextCup = currentCup.Next;
+                currentCup.Next = currentCup.Next.Next.Next.Next;
+                currentCup.Next.Previous = currentCup;
+                int destinationValue = currentCup.Value;
+                do
+                {
+                    destinationValue--;
+                    if (destinationValue < minValue)
+                    {
+                        destinationValue = maxValue;
+                    }
+                }
+                while (destinationValue == nextCup.Value || destinationValue == nextCup.Next.Value || destinationValue == nextCup.Next.Next.Value);
+                Cup destinationCup = cups[destinationValue];
+                Cup afterDestinationCup = destinationCup.Next;
+                destinationCup.Next = nextCup;
+                nextCup.Previous = destinationCup;
+                afterDestinationCup.Previous = nextCup.Next.Next;
+                nextCup.Next.Next.Next = afterDestinationCup;
+                currentCup = currentCup.Next;
+            }
+
+            return cups[1];
+        }
+
+        // First attempt was fast enough for part one, but not part two.
         private static List<int> MoveCups(List<int> originalCups, int numberOfMoves)
         {
             List<int> cups = originalCups.ToList();
             int currentCup = cups.ElementAt(0);
+            int cupCount = cups.Count();
+            int cupMin = cups.Min();
+            int cupMax = cups.Max();
 
             for (int moves = 1; moves <= numberOfMoves; moves++)
             {
                 int currentCupIndex = cups.IndexOf(currentCup);
-                //Console.WriteLine($"Move {moves}");
-                //Console.WriteLine($"Current cup value: {currentCup}");
-                //Console.WriteLine($"Current cup position: {currentCupIndex + 1}");
-                //Console.WriteLine($"Current cups: {string.Join(", ", cups)}");
                 List<int> pickupCups = new List<int>();
                 for (int pickupCount = 1; pickupCount <= 3; pickupCount++)
                 {
-                    int pickupCup = cups[(currentCupIndex + pickupCount) % cups.Count()];
+                    int pickupCup = cups[(currentCupIndex + pickupCount) % cupCount];
                     pickupCups.Add(pickupCup);
                 }
-                //Console.WriteLine($"Pickup cups: {string.Join(", ", pickupCups)}");
                 int destinationCup = currentCup - 1;
-                cups.RemoveAll(c => pickupCups.Contains(c));
+                foreach (int cup in pickupCups)
+                {
+                    cups.Remove(cup);
+                }
                 while (!cups.Contains(destinationCup))
                 {
                     destinationCup--;
-                    if (destinationCup < cups.Min())
+                    if (destinationCup < cupMin)
                     {
-                        destinationCup = cups.Max();
+                        destinationCup = cupMax;
                     }
                 }
                 int destinationCupIndex = cups.IndexOf(destinationCup);
-                //Console.WriteLine($"Destination: {destinationCup}");
                 cups.InsertRange(destinationCupIndex + 1, pickupCups);
-                currentCup = cups[(cups.IndexOf(currentCup) + 1) % cups.Count()];
-                //Console.WriteLine();
+                currentCup = cups[(cups.IndexOf(currentCup) + 1) % cupCount];
             }
 
             return cups;
