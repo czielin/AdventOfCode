5651ec453e2c63fe090026e71649806542830051
Chris Zielin <chris@zielin.com>
2021-01-23 08:29:37 -0800

    Day 24, part 1.

---
+++ Day24/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day24
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+
+            Tile referenceTile = new Tile(0, 0);
+
+            foreach (string line in lines)
+            {
+                Tile currentTile = referenceTile;
+                for (int index = 0; index < line.Length; index++)
+                {
+                    char direction = line[index];
+                    if (direction == 'n')
+                    {
+                        index++;
+                        char subDirection = line[index];
+                        if (subDirection == 'e')
+                        {
+                            currentTile = currentTile.NorthEast;
+                        }
+                        else
+                        {
+                            currentTile = currentTile.NorthWest;
+                        }
+                    }
+                    else if (direction == 's')
+                    {
+                        index++;
+                        char subDirection = line[index];
+                        if (subDirection == 'e')
+                        {
+                            currentTile = currentTile.SouthEast;
+                        }
+                        else
+                        {
+                            currentTile = currentTile.SouthWest;
+                        }
+                    }
+                    else if (direction == 'e')
+                    {
+                        currentTile = currentTile.East;
+                    }
+                    else
+                    {
+                        currentTile = currentTile.West;
+                    }
+                }
+
+                currentTile.Flip();
+            }
+
+            Console.WriteLine($"Black tiles: { Tile.TileLocations.Values.Count(t => t.Color == Color.Black) }");
+        }
+    }
+}
---
+++ Day24/Tile.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Day24
+{
+    public class Tile
+    {
+        private readonly int x;
+        private readonly int y;
+
+        public Tile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Dictionary<(int x, int y), Tile> TileLocations = new Dictionary<(int x, int y), Tile>();
+        public Color Color { get; set; } = Color.White;
+        public Tile East
+        {
+            get
+            {
+                if (!TileLocations.ContainsKey((x + 2, y)))
+                {
+                    TileLocations.Add((x + 2, y), new Tile(x + 2, y));
+                }
+                return TileLocations[(x + 2, y)];
+            }
+        }
+        public Tile SouthEast
+        {
+            get
+            {
+                if (!TileLocations.ContainsKey((x + 1, y - 1)))
+                {
+                    TileLocations.Add((x + 1, y - 1), new Tile(x + 1, y - 1));
+                }
+                return TileLocations[(x + 1, y - 1)];
+            }
+        }
+        public Tile SouthWest
+        {
+            get
+            {
+                if (!TileLocations.ContainsKey((x - 1, y - 1)))
+                {
+                    TileLocations.Add((x - 1, y - 1), new Tile(x - 1, y - 1));
+                }
+                return TileLocations[(x - 1, y - 1)];
+            }
+        }
+        public Tile West
+        {
+            get
+            {
+                if (!TileLocations.ContainsKey((x - 2, y)))
+                {
+                    TileLocations.Add((x - 2, y), new Tile(x - 2, y));
+                }
+                return TileLocations[(x - 2, y)];
+            }
+        }
+        public Tile NorthWest
+        {
+            get
+            {
+                if (!TileLocations.ContainsKey((x - 1, y + 1)))
+                {
+                    TileLocations.Add((x - 1, y + 1), new Tile(x - 1, y + 1));
+                }
+                return TileLocations[(x - 1, y + 1)];
+            }
+        }
+        public Tile NorthEast
+        {
+            get
+            {
+                if (!TileLocations.ContainsKey((x + 1, y + 1)))
+                {
+                    TileLocations.Add((x + 1, y + 1), new Tile(x + 1, y + 1));
+                }
+                return TileLocations[(x + 1, y + 1)];
+            }
+        }
+        public void Flip()
+        {
+            Color = Color == Color.White ? Color.Black : Color.White;
+        }
+    }
+}
