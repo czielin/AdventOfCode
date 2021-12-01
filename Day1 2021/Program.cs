25978dd9c2b9690366e77ca17e1815d42078ab6d
Chris Zielin <chris@zielin.com>
2021-12-01 14:16:23 -0800

    Day 1, part 2.

--- Day1 2021/Program.cs
+++ Day1 2021/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Day1_2021
@@ -11,7 +13,9 @@ namespace Day1_2021
             var lines = await File.ReadAllLinesAsync("input.txt");
 
             int previousMeasurement = int.MaxValue;
+            int twoMeasurementsAgo = int.MaxValue;
             int increases = 0;
+            List<int> slidingMeasurements = new List<int>();
 
             for (int lineCount = 0; lineCount < lines.Length; lineCount++)
             {
@@ -20,10 +24,26 @@ namespace Day1_2021
                 {
                     increases++;
                 }
+                if (twoMeasurementsAgo != int.MaxValue)
+                {
+                    slidingMeasurements.Add(currentMeasurement + previousMeasurement + twoMeasurementsAgo);
+                }
+                twoMeasurementsAgo = previousMeasurement;
                 previousMeasurement = currentMeasurement;
             }
 
             Console.WriteLine($"The number of increases is: {increases}");
+            increases = 0;
+
+            for (int measurementCount = 0; measurementCount < slidingMeasurements.Count() - 1; measurementCount++)
+            {
+                if (slidingMeasurements.ElementAt(measurementCount + 1) > slidingMeasurements.ElementAt(measurementCount))
+                {
+                    increases++;
+                }
+            }
+
+            Console.WriteLine($"The number of sliding increases is: {increases}");
         }
     }
 }
