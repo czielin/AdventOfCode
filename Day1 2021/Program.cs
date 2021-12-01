5d6094e48c6fb32ce6f9ec18f06c2c2f342610a9
Chris Zielin <chris@zielin.com>
2021-12-01 10:23:29 -0800

    Day 1, part 1.

---
+++ Day1 2021/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Day1_2021
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+
+            int previousMeasurement = int.MaxValue;
+            int increases = 0;
+
+            for (int lineCount = 0; lineCount < lines.Length; lineCount++)
+            {
+                int currentMeasurement = int.Parse(lines[lineCount]);
+                if (currentMeasurement > previousMeasurement)
+                {
+                    increases++;
+                }
+                previousMeasurement = currentMeasurement;
+            }
+
+            Console.WriteLine($"The number of increases is: {increases}");
+        }
+    }
+}
