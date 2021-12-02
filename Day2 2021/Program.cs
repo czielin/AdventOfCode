ef60333459e26c8135a0b925ca97a2adc6af99bf
Chris Zielin <chris@zielin.com>
2021-12-01 21:48:09 -0800

    Day 2, part 1.

---
+++ Day2 2021/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Day2_2021
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+
+            int horizontalPosition = 0;
+            int depth = 0;
+
+            for (int lineCount = 0; lineCount < lines.Length; lineCount++)
+            {
+                var lineParts = lines[lineCount].Split(' ');
+                string action = lineParts[0];
+                int units = int.Parse(lineParts[1]);
+
+                switch (action)
+                {
+                    case "forward":
+                        horizontalPosition += units;
+                        break;
+                    case "down":
+                        depth += units;
+                        break;
+                    case "up":
+                        depth -= units;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Depth * horizontal position: {depth * horizontalPosition}");
+        }
+    }
+}
