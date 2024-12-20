dfda287b67d7fc664d78c933737ac346e685a959
Zielin, Chris <chris.zielin@gdit.com>
2024-12-19 22:52:27 -0800

    2024 Day 3, Part 2.

--- 2024/Day3/Program.cs
+++ 2024/Day3/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System.Text.RegularExpressions;
 
+string regex = "(mul\\((?<multiplicand>\\d{1,3}),(?<multiplier>\\d{1,3})\\))|do\\(\\)|don't\\(\\)";
 var input = File.ReadAllText("input.txt");
-var matches = Regex.Matches(input, "mul\\((?<multiplicand>\\d{1,3}),(?<multiplier>\\d{1,3})\\)");
+var matches = Regex.Matches(input, regex);
 int productSum = 0;
+int conditionalProductSum = 0;
+bool enabled = true;
 foreach (Match match in matches)
 {
-    int multiplicand = int.Parse(match.Groups["multiplicand"].Value);
-    int multiplier = int.Parse(match.Groups["multiplier"].Value);
-    productSum += multiplicand * multiplier;
+    if (match.Value == "do()")
+    {
+        enabled = true;
+    }
+    else if (match.Value == "don't()")
+    {
+        enabled = false;
+    }
+    else
+    {
+        int multiplicand = int.Parse(match.Groups["multiplicand"].Value);
+        int multiplier = int.Parse(match.Groups["multiplier"].Value);
+        productSum += multiplicand * multiplier;
+        if (enabled)
+        {
+            conditionalProductSum += multiplicand * multiplier;
+        }
+    }
+    
 }
 Console.WriteLine($"Sum of products: {productSum}");
+Console.WriteLine($"Conditional sum of products: {conditionalProductSum}");
+Console.ReadLine();
