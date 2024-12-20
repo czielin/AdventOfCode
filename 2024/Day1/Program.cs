3e71bd1f449b8cee29959df4b1941251fc7dbee9
Zielin, Chris <chris.zielin@gdit.com>
2024-12-19 22:14:50 -0800

    2024 Days 1 and 2.

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            int totalDifference = 0;
+            int similarityScore = 0;
+            var left = new int[lines.Length];
+            var right = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var parts = lines[i].Split("   ");
+                left[i] = int.Parse(parts[0]);
+                right[i] = int.Parse(parts[1]);
+            }
+            var sortedLeft = left.Order();
+            var sortedRight = right.Order();
+            for (int i = 0; i < left.Length; i++)
+            {
+                totalDifference += Math.Abs(sortedRight.ElementAt(i) - sortedLeft.ElementAt(i));
+                similarityScore += left[i] * right.Count(r => r == left[i]);
+            }
+            Console.WriteLine($"Total difference: {totalDifference}");
+            Console.WriteLine($"Similarity score: {similarityScore}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 2024/Day2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+
+bool isReportSafe(IEnumerable<int> levels)
+{
+    bool increasing = levels.ElementAt(0) < levels.ElementAt(1);
+    bool isSafe = true;
+    for (int i = 0; i < levels.Count() - 1; i++)
+    {
+        int difference = levels.ElementAt(i + 1) - levels.ElementAt(i);
+        int absoluteDiffence = Math.Abs(difference);
+
+        if
+        (
+            (increasing && difference > 0 || !increasing && difference < 0)
+            && absoluteDiffence >= 1
+            && absoluteDiffence <= 3
+        )
+        {
+            // safe.
+        }
+        else
+        {
+            isSafe = false;
+            break;
+        }
+    }
+    return isSafe;
+}
+
+bool isDampenedReportSafe(List<int> levels)
+{
+    bool isSafe = false;
+    for (int i = 0; i < levels.Count && !isSafe; i++)
+    {
+        var levelsCopy = new List<int>(levels);
+        levelsCopy.RemoveAt(i);
+        isSafe = isReportSafe(levelsCopy);
+    }
+    return isSafe;
+}
+
+var lines = File.ReadAllLines("input.txt");
+int safeCount = 0;
+int dampenedSafeCount = 0;
+foreach (var line in lines)
+{
+    var levels = line.Split(' ').Select(l => int.Parse(l)).ToList();
+
+    if (isReportSafe(levels))
+    {
+        safeCount++;
+        dampenedSafeCount++;
+    }
+    else if (isDampenedReportSafe(levels))
+    {
+        dampenedSafeCount++;
+    }
+}
+Console.WriteLine($"Safe count: {safeCount}");
+Console.WriteLine($"Dampened safe count: {dampenedSafeCount}");
+Console.ReadLine();
