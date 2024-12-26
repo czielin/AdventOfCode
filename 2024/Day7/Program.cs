0be4bfc10c7b0806052e9434cba95c859b0007c9
Zielin, Chris <chris.zielin@gdit.com>
2024-12-25 23:07:06 -0800

    2024, Day 7 part 2.

--- 2024/Day7/Program.cs
+++ 2024/Day7/Program.cs
@@ -1,33 +1,60 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 long validLinesSum = 0;
+long validLinesThreeOperatorsSum = 0;
 
 foreach (var line in lines)
 {
     var lineParts  = line.Split(' ');
     long answer = long.Parse(lineParts[0].Trim(':'));
     var digits = lineParts[1..].Select(d => int.Parse(d)).ToList();
-    for (int operatorCounter = 0; operatorCounter < Math.Pow(2, digits.Count) - 1; operatorCounter++)
+
+    if (IsEquationValid(answer, digits, 2))
     {
-        long result = digits[0];
-        for (int digitCount = 1; digitCount < digits.Count; digitCount++)
-        {
-            int operatorType = operatorCounter & (int)Math.Pow(2, digitCount - 1);
-            if (operatorType == 0)
-            {
-                result += digits[digitCount];
-            }
-            else
-            {
-                result *= digits[digitCount];
-            }
-        }
-        if (result == answer)
-        {
-            validLinesSum += answer;
-            break;
-        }
+        validLinesSum += answer;
+    }
+
+    if (IsEquationValid(answer, digits, 3))
+    {
+        validLinesThreeOperatorsSum += answer;
     }
 }
 
 Console.WriteLine($"The sum of valid lines is: {validLinesSum}");
+Console.WriteLine($"The sum of valid lines with three operators is: {validLinesThreeOperatorsSum}");
 Console.ReadLine();
+
+bool IsEquationValid(long answer, List<int> digits, int numberOfOperators)
+{
+    bool isValid = false;
+    for (int operatorCounter = 0; operatorCounter < Math.Pow(numberOfOperators, digits.Count - 1); operatorCounter++)
+    {
+        long result = digits[0];
+        long amountUsed = 0;
+        for (int digitCount = digits.Count - 1; digitCount > 0; digitCount--)
+        {
+            long remainingAmount = operatorCounter - amountUsed;
+            int operatorType = (int)Math.Floor((double)remainingAmount / Math.Pow(numberOfOperators, digitCount - 1));
+            amountUsed += operatorType * (long)Math.Pow(numberOfOperators, digitCount - 1);
+            int nextDigitIndex = digits.Count - digitCount;
+            if (operatorType == 0)
+            {
+                result += digits[nextDigitIndex];
+            }
+            else if (operatorType == 1)
+            {
+                result *= digits[nextDigitIndex];
+            }
+            else
+            {
+                string concat = result.ToString() + digits[nextDigitIndex];
+                result = long.Parse(concat);
+            }
+        }
+        if (result == answer)
+        {
+            isValid = true;
+            break;
+        }
+    }
+    return isValid;
+}
